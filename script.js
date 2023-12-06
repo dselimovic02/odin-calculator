a7c470febec73062e6ba146cb74f5ca4d8d34a6f
dselimovic <dselimovic02@gmail.com>
2023-12-06 17:27:39 +0100

    Display operations

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 const input = document.querySelector(".input");
 const output = document.querySelector(".output");
 const numbers = document.querySelectorAll("button.num");
-const operators = document.querySelectorAll("operators");
+const operators = document.querySelectorAll(".operator");
+const negative = document.querySelector("button.negative");
 const clear = document.querySelector("button#clear");
 const del = document.querySelector("button#delete");
 const equals = document.querySelector("button.equals");
 const dot = document.querySelector(".dot");
+const operation = {
+    add: '<i class="fa-solid fa-plus"></i>',
+    subtract: '<i class="fa-solid fa-minus"></i>',
+    multiply: '<i class="fa-solid fa-xmark"></i>',
+    divide: '<i class="fa-solid fa-divide"></i>'
+};
 
 numbers.forEach(number =>{
     number.addEventListener("click", () => {
@@ -24,13 +31,10 @@ dot.addEventListener("click", () => {
     }
 });
 
-clear.addEventListener("click", () => {
-    input.textContent = "0";
-});
+clear.addEventListener("click", clearAll);
 
 del.addEventListener("click", () => {
-    console.log(input.textContent.length);
-    if(input.textContent.length < 2){
+    if( (input.textContent.length < 2) || (input.textContent.length < 3 && input.textContent.includes("-")) ){
         input.textContent = "0";
     }else{
         let newValue = input.textContent.split('');
@@ -38,3 +42,31 @@ del.addEventListener("click", () => {
         input.textContent = newValue.join('');    
     }
 });
+
+negative.addEventListener("click", () =>{
+    let newValue = input.textContent.split('');
+    if(!input.textContent.includes("-")){
+        newValue.unshift('-');
+    }else{
+        newValue.shift();
+    }
+    input.textContent = newValue.join('');
+});
+
+operators.forEach(operator => {
+    operator.addEventListener("click", () => {
+        output.innerHTML = input.textContent + ` ${operation[operator.id]}`;
+        clearInput();
+    });
+});
+
+function clearInput(){
+    input.textContent = "0";
+}
+function clearOutput(){
+    output.textContent = "";
+}
+function clearAll(){
+    clearInput();
+    clearOutput();
+}
